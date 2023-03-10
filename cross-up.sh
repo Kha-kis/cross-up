7abc2f5117bc07d9dc2c106f857569f269d4ad94
Khakis <27432909+Kha-kis@users.noreply.github.com>
2023-03-10 10:36:56 -0600

    Update cross-up.sh

--- cross-up.sh
+++ cross-up.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# Cross-Up an auto reupload and cross-seed script for use with qbittorrent created by khak1s
-# This tool relies on cross-seed Autobrr/RSS feeds, and L4G Upload-Assistant
-#
-#
-#Cross-seed must be set up in Daemon mode in order for this script to work
-#This goes into the 'Run external program on torrent completion' section of qbittorrent:
-#/path/to/crossup.sh "%F" "%L" "%N"
-#This next section passes through the parameter from the command line by order.
-#
-# %F is the content path
-# %L is the catagory for the torrents you want to reupload
-# %N is the name of the torrent to allow for cross seeding
-#
-#%F=$1
-#%L=$2
-#%N=$3
-
 source config.cfg
 
 exec >> $logfile 2>&1
