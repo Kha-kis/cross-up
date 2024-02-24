426ce5835a39b370481057cd9a122d3b8cfe47f9
Khakis <jacobneighbors@gmail.com>
2024-02-24 16:47:13 -0600

    Update cross-up.py
    add support for rtorrent

--- cross-up.py
+++ cross-up.py
@@ -39,20 +39,38 @@ logger.info('')
 
 baseupload_cmd = ['python3', config['l4g'], sys.argv[1], '-ua', '-tk', config['trackers']]
 
-# Check if sys.argv[2] matches config['cat']
-if sys.argv[2] == config['cat']:
-    try:
-        # Run L4G Upload Assistant when sys.argv[2] matches config['cat']
-        upload_cmd = baseupload_cmd + [config['upload_args']]
-        logger.info('Running L4G Upload Assistant: %s', ' '.join(upload_cmd))
-        upload_result = subprocess.run(upload_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        logger.info('L4G Upload Assistant output: %s', upload_result.stdout.decode('utf-8').strip())
-    except subprocess.CalledProcessError as e:
-        logger.error('Error running command: %s', e)
-        print('Error running command:', e)
+# Inside the conditional block for handling the upload
+if config['torrent_client'] == 'qbittorrent':
+    if sys.argv[2] == config['cat']:
+        try:
+            # Run L4G Upload Assistant when sys.argv[2] matches config['cat']
+            upload_cmd = baseupload_cmd + [config['upload_args']]
+            logger.info('Running L4G Upload Assistant for qBittorrent: %s', ' '.join(upload_cmd))
+            upload_result = subprocess.run(upload_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            logger.info('L4G Upload Assistant output: %s', upload_result.stdout.decode('utf-8').strip())
+        except subprocess.CalledProcessError as e:
+            logger.error('Error running command: %s', e)
+            print('Error running command:', e)
+    else:
+        logger.error('Unknown error')
+        print('Unknown error')
+elif config['torrent_client'] == 'rtorrent':
+    if sys.argv[2] == config['label']:
+        try:
+            # Run L4G Upload Assistant when sys.argv[2] matches config['label']
+            upload_cmd = baseupload_cmd + [config['upload_args']]
+            logger.info('Running L4G Upload Assistant for rTorrent: %s', ' '.join(upload_cmd))
+            upload_result = subprocess.run(upload_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            logger.info('L4G Upload Assistant output: %s', upload_result.stdout.decode('utf-8').strip())
+        except subprocess.CalledProcessError as e:
+            logger.error('Error running command: %s', e)
+            print('Error running command:', e)
+    else:
+        logger.error('Unknown error')
+        print('Unknown error')
 else:
-    logger.error('Unknown error')
-    print('Unknown error')
+    logger.error('Unknown torrent client specified in config')
+    print('Unknown torrent client specified in config')
 
 # Trigger cross-seed (outside of the if block)
 cs_cmd = [
